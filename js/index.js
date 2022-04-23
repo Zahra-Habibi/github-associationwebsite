a266c6dc2247d14dd98ea58b846523b1c83a81db
ZahraHabibi <habibi.zahra.ku@gmail.com>
2022-04-23 01:19:07 -0700

    the first commit

---
+++ js/index.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+  $("#overlay").fadeOut();
+  keepTheme();
+
+  // the  owl carousel for the partners company
+  var owl = $(".partnerCarousel");
+  owl.owlCarousel({
+    items: 4,
+    rtl: true,
+    margin: 25,
+    nav: true,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+      1200: {
+        items: 5,
+      },
+    },
+  });
+});
+
+// handling the dark mood
+
+const keepTheme = () => {
+  const toNight = () => {
+    $("body").addClass("darkMod");
+    $(".DayIcon").removeClass("isDay");
+    $(".NightIcon").addClass("isNight");
+  };
+  const toDay = () => {
+    $("body").removeClass("darkMod");
+    $(".DayIcon").addClass("isDay");
+    $(".NightIcon").removeClass("isNight");
+  };
+  const Data = window.localStorage.getItem("DayNight");
+  if (Data === null) {
+    window.localStorage.setItem("DayNight", "Day");
+  }
+  if (Data === "Day") {
+    toDay();
+    window.localStorage.setItem("DayNight", "Day");
+  } else {
+    toNight();
+    window.localStorage.setItem("DayNight", "Night");
+  }
+};
+
+// *********************  end of dark mood **************************//
+
+// show menubar
+const showMenu = () => {
+  document.querySelector(".menubar_sm").classList.remove("smMenu");
+  document.querySelector(".menubar_sm").classList.add("showMenu");
+  let myArray = document.querySelectorAll(".myList");
+  addListItem(myArray);
+};
+
+// close menubar in small devices
+const closeIcon = () => {
+  document.querySelector(".menubar_sm").classList.remove("showMenu");
+  document.querySelector(".menubar_sm").classList.add("smMenu");
+  let myArray = document.querySelectorAll(".showList");
+  removeListItem(myArray);
+};
+
+// addList function
+const addListItem = (array) => {
+  for (let x of array) {
+    x.classList.remove("myList");
+    x.classList.add("showList");
+  }
+};
+// remove function
+const removeListItem = (array) => {
+  for (let x of array) {
+    x.classList.remove("showList");
+    x.classList.add("myList");
+  }
+};
+
